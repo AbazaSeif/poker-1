ece99cc219f29a6d886cccbc00aa569b5c816540
alanrgan <alanrgan@gmail.com>
2015-11-25 22:17:20 -0500

    Update checkturn.php

--- poker/checkturn.php
+++ poker/checkturn.php
@@ -1,5 +1,5 @@
 <?php
-	$con = new mysqli("173.71.126.184:3306","root","password", "pokerdb");
+	$con = new mysqli("localhost","root","password", "pokerdb");
 	if ($con->connect_errno) {
 		printf("Connect failed: %s\n", mysqli_connect_error());
 		exit();
