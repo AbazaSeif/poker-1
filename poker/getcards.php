e7bcbf5808d6d6357e3b0abe2787c8c75ca36251
alanrgan <alanrgan@gmail.com>
2015-11-25 22:12:32 -0500

    Update getcards.php

--- poker/getcards.php
+++ poker/getcards.php
@@ -1,21 +1,28 @@
 <?php
-$con = mysql_connect("173.71.126.184:3306","alanrgan","penguins666");
+$con = mysql_connect("173.71.126.184:3306","root","password");
+
 if (!$con)
 {
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("pokerdb", $con);
-$name = $_POST['name'];
-if($name != "") {
-    $getdeck = mysql_query("SELECT * FROM game_table WHERE player2='$name'") or die(mysql_error());	
+mysql_select_db("pokerdb", $con);
+
+$name = $_POST['name'];
+
+if($name != "") {
+
+    $getdeck = mysql_query("SELECT * FROM game_table WHERE player2='$name'") or die(mysql_error());
+	
     while($row = mysql_fetch_array($getdeck)) {
         $deck = $row['deck'];
         $opponent = $row['player1'];
-    }	
+    }
+	
     $array = array();
     $array["deck"] = $deck;
     $array["opponent"] = $opponent;
     echo json_encode($array);
 }
-mysql_close($con);
+mysql_close($con);
+
 ?>
