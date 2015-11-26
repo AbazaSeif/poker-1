09d1a29ae258e7c82260c4e1766fad233fe17ecd
alanrgan <alanrgan@gmail.com>
2015-11-25 22:16:32 -0500

    Update updatedeck.php

--- poker/updatedeck.php
+++ poker/updatedeck.php
@@ -1,18 +1,26 @@
-<?php
+<?php
+
 $name = $_POST['name'];
-$con = mysql_connect("173.71.126.184:3306","alanrgan","penguins666");
+$con = mysql_connect("localhost","root","password");
+
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
-}
-mysql_select_db("pokerdb", $con);
+}
+
+mysql_select_db("pokerdb", $con);
+
 $deck = $_POST['deck'];
-$name = $_POST['name'];
+$name = $_POST['name'];
+
 if($name != "") {
     if($deck != "") {
         mysql_query("UPDATE game_table SET deck='$deck' WHERE player2='$name' OR player1='$name'");
-    } else {		echo "deck not set up";
+    } else {
+		echo "deck not set up";
     }
-}
-mysql_close($con);
+}
+
+mysql_close($con);
+
 ?>
